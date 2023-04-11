62e3236f14532ee89dbb751a92ea90e751469cc7
Mysticul05 <130502277+Mysticul05@users.noreply.github.com>
2023-04-11 12:22:28 -0700

    Updated test.py
    First draft test.py to draw a house

--- test.py
+++ test.py
@@ -1,1 +1,34 @@
-nothing
+from turtle import *
+from time import sleep
+
+#house
+pu(
+)
+forward(100)
+pd()
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+
+#roof
+penup()
+goto(50,0)
+pd()
+left(90)
+pu()
+forward(100)
+pd()
+right(140)
+forward(125)
+left(140)
+left(140)
+forward(125)
+left(90)
+left(130)
+forward(50)
+
+
+sleep (10)
